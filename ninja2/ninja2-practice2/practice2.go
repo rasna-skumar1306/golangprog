e22c45b24f18647688002165424ee0c5cadea49b
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-02 00:02:52 +0530

    boolean program

---
+++ ninja2/ninja2-practice2/practice2.go
@@ -0,0 +1,17 @@
+//program that prints boolean for various conditional operations
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := (42 == 42)
+	b := (42 <= 43)
+	c := (42 >= 43)
+	d := (42 != 43)
+	e := (42 < 43)
+	f := (42 > 43)
+	fmt.Println(a, b, c, d, e, f)
+}
