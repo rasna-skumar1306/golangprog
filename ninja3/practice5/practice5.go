12910272fd9f594a92c414e8fad3311ae7d80785
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-02 20:33:20 +0530

    simple if-else

---
+++ ninja3/practice5/practice5.go
@@ -0,0 +1,14 @@
+//simple if-else program
+package main
+
+import "fmt"
+
+func main() {
+	if 1+1 == 3 {
+		fmt.Println("1 + 1 = 3")
+	} else if 1+1 == 2 {
+		fmt.Println("1+1=2")
+	} else {
+		fmt.Println("both are false")
+	}
+}
