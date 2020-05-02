8101dd9520601cd8ed78ed772bfc6bd1d9439b2d
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-02 20:33:47 +0530

    switch case without condition

---
+++ ninja3/practice6/practice6.go
@@ -0,0 +1,13 @@
+//program to print true or false using switch case without condition
+package main
+
+import "fmt"
+
+func main() {
+	switch {
+	case true:
+		fmt.Println("output is true")
+	case false:
+		fmt.Println("output is false")
+	}
+}
