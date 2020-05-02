5844804bfe2bff4fa09dc42c52f7e5027eed6ab7
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-02 18:03:46 +0530

    for [condition] {code} format

---
+++ ninja3/practice3/practice3.go
@@ -0,0 +1,12 @@
+//this program uses for in while format
+package main
+
+import "fmt"
+
+func main() {
+	y := 2000
+	for y <= 2020 { //here for [condition] { code } format is used
+		fmt.Println(y)
+		y++
+	}
+}
