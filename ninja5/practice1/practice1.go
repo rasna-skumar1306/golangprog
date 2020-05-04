7c0884e5a039dda0ea27dab58c1834ea6816e74b
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-04 11:36:05 +0530

    struct with slice

---
+++ ninja5/practice1/practice1.go
@@ -0,0 +1,41 @@
+//program that combines struct with slice
+package main
+
+import "fmt"
+
+type nume struct {	//declaration of struct
+	first string
+	last  string
+	fav   []string
+}
+
+func main() {
+	p1 := nume{
+		first: "James",
+		last:  "Bond",
+		fav: []string{
+			"vodka",
+			"martini",
+			"vesper",
+		},
+	}
+	p2 := nume{
+		first: "Miss",
+		last:  "Moneypenny",
+		fav: []string{
+			"chocolate",
+			"gin",
+			"flowers",
+		},
+	}
+	fmt.Println(p1.first)
+	fmt.Println(p1.last)
+	for i, v := range p1.fav {
+		fmt.Printf("%v\t%v\n", i, v)
+	}
+	fmt.Println(p2.first)
+	fmt.Println(p2.last)
+	for i, v := range p2.fav {
+		fmt.Printf("%v\t%v\n", i, v)
+	}
+}
