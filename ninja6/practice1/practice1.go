70f45095ad99ed855d5d6f0e6cd9de544d0c1258
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-07 00:47:33 +0530

    Changes done to problem 1

--- ninja6/practice1/practice1.go
+++ ninja6/practice1/practice1.go
@@ -1,13 +1,14 @@
+//program to use return values in functions
 package main
 
 import "fmt"
 
-func foo() int {
+func foo() int { //function that returns an int
 	x := 32
 	return x
 }
 
-func bar() (int, string) {
+func bar() (int, string) { //function that returns an int and a string
 	x := 123
 	r := "rasna"
 	return x, r
