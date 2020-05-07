e0668373feecdd65e96fe021fdad2ad8ab1b97ee
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-07 23:49:45 +0530

    scope of a variable

---
+++ ninja6/practice10/practice10.go
@@ -0,0 +1,27 @@
+//exploring scope of a variable using incrementor function
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	f := foo()
+	fmt.Println(f())
+	fmt.Println(f())
+	g := foo()
+	fmt.Println(g())
+	fmt.Println(g())
+	fmt.Println(g())
+	fmt.Println(g())
+	fmt.Println(g())
+	fmt.Println(g())
+}
+
+func foo() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
