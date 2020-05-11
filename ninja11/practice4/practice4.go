501253ad743ccd0df4071df3a4568bfccd5cefdb
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-11 14:53:08 +0530

    custom error using function

---
+++ ninja11/practice4/practice4.go
@@ -0,0 +1,33 @@
+//custom error using function
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		e := errors.New("still need more clarity")
+		return 0, sqrtError{"50.2289 N", "99.4656 W", e}
+	}
+	return 42, nil
+}
